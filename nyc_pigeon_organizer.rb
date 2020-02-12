d5410df6534835b07df732e5cd477c328d922cc9
Ben Hickson <bzhickson@gmail.com>
2020-02-12 04:29:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,50 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+#   # p data
+
+
+  # names = {}
+  
+  # why doesnt this work??
+  
+  # data.each do |key1, value1|       #loop the attributes
+  #   attribute = key1
+  #   value1.each do |key2, value2|    #loop the values
+  #     value = key2
+  #     value2.each do |name|
+  #       # names[name][attribute.to_sym] << value.to_s
+  #       names[name][attribute.to_sym].push(value.to_s)
+  #     end
+  #   end
+  # end
+  
+  # names
+
+
+
+
+  names_array = []
+  output_hash = {}
+
+  data.each do |attribute, items|
+    items.each do |feature, arr|
+      arr.each do |name|
+        names_array << name if !names_array.include?(name)
+      end
+    end
+  end
+  
+  names_array.each do |name_from_namearray|
+    output_hash[name_from_namearray] = Hash.new {|k, v| k[v] = []}
+    data.each do |attribute, items|
+      output_hash[name_from_namearray][attribute]
+      items.each do |feature, arr|
+        arr.each do |name|
+          output_hash[name][attribute] << feature.to_s if name == name_from_namearray
+        end
+      end
+    end
+  end
+  
+  output_hash
+  
 end
