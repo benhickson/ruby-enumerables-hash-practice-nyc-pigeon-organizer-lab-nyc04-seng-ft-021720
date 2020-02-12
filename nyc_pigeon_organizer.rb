24bc3732bac6978e3ec7b0b8906463856132ce9f
Ben Hickson <bzhickson@gmail.com>
2020-02-12 04:24:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,8 @@ def nyc_pigeon_organizer(data)
     value1.each do |key2, value2|    #loop the values
       value = key2
       value2.each do |name|
-        names[name][attribute.to_sym] << value.to_s
+        # names[name][attribute.to_sym] << value.to_s
+        names[name][attribute.to_sym].push(value.to_s)
       end
     end
   end
